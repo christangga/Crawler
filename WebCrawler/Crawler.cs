4477d37c94fe864d2c4dfbbf971569ff5b17ae9f
Christ Angga Saputra <christanggasaputra@gmail.com>
2014-03-26 10:41:36 +0700

    crawling max pages

--- WebCrawler/Crawler.cs
+++ WebCrawler/Crawler.cs
@@ -1,5 +1,4 @@
 ﻿using HtmlAgilityPack;
-using System.Data.SQLite;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,52 +10,45 @@ namespace WebCrawler {
 	class Crawler {
 		private const int BFS_MAX_DEPTH = 2;
 		private const int DFS_MAX_DEPTH = 2;
-		
-		private char[] delimiterChars = {' ', ',', '.', ':', '\t', '\n', '?', '\\', '/', '!', '@', '\r', '(', ')', '\'', '\"', '<', '>', '[', ']', '{', '}', '|', '”', '“', '~', '#', '&', '-'};
-		private LinkedList<String> urlsToVisit;
-		private HashSet<String> urlsVisited;
-		private StreamWriter swCrawl, swIndex, swErrLog;
+		private const int BFS_MAX_PAGES = 1000;
+		private const int DFS_MAX_PAGES = 1000;
 
-		MySqlConnection sql;
-		MySqlCommand command;
-		public Crawler(String baseUrl, int searchtype) {
-			urlsToVisit = new LinkedList<String>();
-			urlsVisited = new HashSet<String>();
-			sql = new MySqlConnection(@"server=localhost;database=SQL;userid=root;");
-			sql.Open();
+		public static string[] delimiterStrings = { " ", ",", ".", ":", "\t", "\n", "?", "\\", "/", "!", "@", "\r", "(", ")", "\"", "\'", "<", ">", "[", "]", "{", "}", "|", "”", "“", "~", "#", "&", "-" };
+
+		private LinkedList<String> urlsToVisit = new LinkedList<String>();
+		private HashSet<String> urlsVisited = new HashSet<String>();
+		private StreamWriter swCrawl = new StreamWriter("crawling.txt");
+		private StreamWriter swIndex = new StreamWriter("indexing.txt");
+		private StreamWriter swErrLog = new StreamWriter("errlog.txt");
+		private MySqlConnection sql = new MySqlConnection(@"server=localhost;database=SQL;userid=root;");
+		private MySqlCommand command;
+
+		public Crawler(String baseUrl, int crawltype, int maxpages) {
 			//String createDB = "create table if not exists data (URL varchar(200), Title varchar(200), Word varchar(100), UNIQUE (URL, Title, Word) on conflict replace)";
-			command = sql.CreateCommand();
 			//command.CommandText = createDB;
 			//command.ExecuteNonQuery();
-			Console.WriteLine("1. Crawling");
-			Console.WriteLine("2. Export data to MySQL");
-			Console.WriteLine("Else. Begin Searching");
-			string pil = Console.ReadLine();
-			if(pil == "1") {
-				swCrawl = new StreamWriter("crawling.txt");
-				swIndex = new StreamWriter("indexing.txt");
-				swErrLog = new StreamWriter("errlog.txt");
-				//SQLiteConnection.CreateFile("SQL.sqlite");
-				Console.WriteLine("Initializing Crawler . . .");
-				if (searchtype == 0) {
-					crawlBFS(baseUrl);
-				} else if (searchtype == 1) {
-					crawlDFS(baseUrl, 0);
-				}
-				swCrawl.Close();
-				swIndex.Close();
-				swErrLog.Close();
-				Console.WriteLine("Done Crawling . . .");
-				Console.WriteLine("Exporting to MySQL . . .");
-				writeToDB();
+			//SQLiteConnection.CreateFile("SQL.sqlite");
+			sql.Open();
+			command = sql.CreateCommand();
+
+			Console.WriteLine("Initializing Crawler . . .");
+			if (crawltype == 0) {
+				//crawlBFSdepth(baseUrl);
+				crawlBFSpages(baseUrl);
+			} else if (crawltype == 1) {
+				//crawlDFSdepth(baseUrl, 0);
+				crawlDFSpages(baseUrl, 0);
 			}
-			if(pil == "2") {
-				writeToDB();
-			}
-			Console.WriteLine("Ready for Searching");
-			search();
+			Console.WriteLine("Done Crawling . . .");
+
+			Console.WriteLine("Exporting to MySQL . . .");
+			writeToDB();
+			Console.WriteLine("Exporting to MySQL done . . .");
+
+			swCrawl.Close();
+			swIndex.Close();
+			swErrLog.Close();
 			sql.Close();
-			
 		}
 
 		private void addUrl(String urlpath) {
@@ -81,7 +73,7 @@ namespace WebCrawler {
 			return null;
 		}
 
-		private void crawlBFS(String baseUrl) {
+		private void crawlBFSdepth(String baseUrl) {
 			swCrawl.WriteLine(">>>>>>>>>> BFS LEVEL 0 <<<<<<<<<<\n");
 			addUrl(baseUrl);
 			swCrawl.WriteLine(urlsVisited.Count + " " + baseUrl + "\n");
@@ -137,7 +129,58 @@ namespace WebCrawler {
 			}
 		}
 
-		private void crawlDFS(String baseUrl, int depth) {
+		private void crawlBFSpages(String baseUrl) {
+			int counter = 0;
+
+			addUrl(baseUrl);
+			++counter;
+			swCrawl.WriteLine(urlsVisited.Count + " " + baseUrl + "\n");
+			foreach (String urlpath in urlsToVisit) {
+				try {
+					String htmlText = getHtmlText(urlpath);
+					swIndex.WriteLine("<LINK>");
+					swIndex.WriteLine(urlpath);
+					swIndex.WriteLine("</>");
+					if (htmlText != null) {
+						HtmlDocument doc = new HtmlDocument();
+						doc.LoadHtml(htmlText);
+
+						// crawling
+						foreach (HtmlNode attNode in doc.DocumentNode.SelectNodes("//a")) {
+							String url = attNode.GetAttributeValue("href", null);
+							if (url != null) {
+								if (isUrlIgnored(url)) {
+									continue;
+								} else {
+									url = new Uri(new Uri(urlpath), url).AbsoluteUri.ToString();
+								}
+								if (!urlsVisited.Contains(url) && counter < BFS_MAX_PAGES) {
+									addUrl(url);
+									Console.WriteLine(urlsVisited.Count);
+									swCrawl.WriteLine(urlsVisited.Count + " " + urlpath + " " + url);
+								}
+							}
+						}
+						HtmlNodeCollection divNodes = doc.DocumentNode.SelectNodes("//div");
+						HtmlNodeCollection comments = doc.DocumentNode.SelectNodes("//comment()");
+
+						if (comments != null) {
+							foreach (HtmlNode comment in comments) {
+								comment.ParentNode.RemoveChild(comment);
+							}
+						}
+
+						// indexing
+						indexing(doc);
+					}
+				} catch (Exception e) {
+					String s = e.ToString();
+					swErrLog.WriteLine(s);
+				}
+			}
+		}
+
+		private void crawlDFSdepth(String baseUrl, int depth) {
 			if (depth > DFS_MAX_DEPTH) {
 
 			} else {
@@ -166,7 +209,7 @@ namespace WebCrawler {
 									Console.WriteLine(urlsVisited.Count);
 									swCrawl.WriteLine(depth + " " + baseUrl + " " + url);
 									if (depth <= DFS_MAX_DEPTH) {
-										crawlDFS(url, depth + 1);
+										crawlDFSdepth(url, depth + 1);
 									}
 								}
 							}
@@ -191,6 +234,60 @@ namespace WebCrawler {
 			
 		}
 
+		private void crawlDFSpages(String baseUrl, int counter) {
+			if (counter > DFS_MAX_PAGES) {
+
+			} else {
+				urlsVisited.Add(baseUrl);
+
+				try {
+					String htmlText = getHtmlText(baseUrl);
+					//Console.WriteLine(baseUrl);
+					swIndex.WriteLine("<LINK>");
+					swIndex.WriteLine(baseUrl);
+					swIndex.WriteLine("</>");
+					if (htmlText != null) {
+						HtmlDocument doc = new HtmlDocument();
+						doc.LoadHtml(htmlText);
+
+						// crawling
+						foreach (HtmlNode attNode in doc.DocumentNode.SelectNodes("//a")) {
+							String url = attNode.GetAttributeValue("href", null);
+							if (url != null) {
+								if (isUrlIgnored(url)) {
+									continue;
+								} else {
+									url = new Uri(new Uri(baseUrl), url).AbsoluteUri.ToString();
+								}
+								if (!urlsVisited.Contains(url)) {
+									Console.WriteLine(urlsVisited.Count);
+									swCrawl.WriteLine(counter + " " + baseUrl + " " + url);
+									if (counter <= DFS_MAX_PAGES) {
+										crawlDFSdepth(url, counter + 1);
+									}
+								}
+							}
+						}
+						HtmlNodeCollection divNodes = doc.DocumentNode.SelectNodes("//div");
+						HtmlNodeCollection comments = doc.DocumentNode.SelectNodes("//comment()");
+
+						if (comments != null) {
+							foreach (HtmlNode comment in comments) {
+								comment.ParentNode.RemoveChild(comment);
+							}
+						}
+
+						// indexing
+						indexing(doc);
+					}
+				} catch (Exception e) {
+					String s = e.ToString();
+					swErrLog.WriteLine(s);
+				}
+			}
+
+		}
+
 		private Boolean isUrlIgnored(String urlpath) {
 			if (urlpath.Contains(".pdf") || urlpath.Contains(".png") || urlpath.Contains(".jpg")) {
 				return true;
@@ -202,7 +299,7 @@ namespace WebCrawler {
 		private void indexing(HtmlDocument doc) {
 			swIndex.WriteLine("<TITLE>");
 			foreach (HtmlNode titleNode in doc.DocumentNode.SelectNodes("//title")) {
-				String[] words = titleNode.InnerText.Split(delimiterChars);
+				String[] words = titleNode.InnerText.Split(delimiterStrings, System.StringSplitOptions.RemoveEmptyEntries);
 				foreach (String word in words) {
 					if (!word.Equals("")) {
 						swIndex.Write(word+ " ");
@@ -213,7 +310,7 @@ namespace WebCrawler {
 			swIndex.WriteLine("</>");
 			swIndex.WriteLine("<WORD>");
 			foreach (HtmlNode div in doc.DocumentNode.SelectNodes("//div")) {
-				String[] words = div.InnerText.Split(delimiterChars);
+				String[] words = div.InnerText.Split(delimiterStrings, System.StringSplitOptions.RemoveEmptyEntries);
 				foreach (String word in words) {
 					if (!word.Equals("")) {
 						swIndex.WriteLine(word);
@@ -222,7 +319,7 @@ namespace WebCrawler {
 
 				foreach (HtmlNode child in div.ChildNodes) {
 					String childContent = child.InnerText;
-					words = childContent.Split(delimiterChars);
+					words = childContent.Split(delimiterStrings, System.StringSplitOptions.RemoveEmptyEntries);
 					using(var trans = sql.BeginTransaction()) {
 						using(var cmd = sql.CreateCommand()) {
 							foreach (String word in words) {
@@ -280,56 +377,6 @@ namespace WebCrawler {
 					}
 				}
 				trans.Commit();
-			}
-		}
-
-		private void search() {
-			Boolean firstWord;
-			String input, query;
-			Console.WriteLine("Masukkan Query: ");
-			input = System.Console.ReadLine();
-			
-			MySqlDataReader reader;
-			while(!input.Equals("exit")) {
-				firstWord = true;
-				int i = 0;
-				String[] listInput = input.Split(delimiterChars);
-				
-				if(listInput.Length == 1) {
-					query = "SELECT URL, Title FROM data WHERE Word LIKE '%" + listInput[0] + "%'";
-					query += "GROUP BY URL ORDER BY URL ASC";
-				}
-				else {
-					query = "SELECT a0.URL, a0.Title FROM ";
-					for(int j = 1; j < listInput.Length; j++ ) {
-						query += "(";
-					}
-					query += " ((SELECT * FROM data WHERE Word LIKE '%" + listInput[0] + "%') a" + i + ")";
-					foreach(String word in listInput){
-						if(firstWord){
-							firstWord = false;
-						}
-						else {
-							i++;
-							query += " INNER JOIN ((SELECT b"+i+".* FROM data b"+i;
-							query += " WHERE Word LIKE '%" + word + "%') a" + i;
-							i++;
-							query += " ) ON a0.URL=a" + (i - 1) + ".URL)";
-						}
-					}
-					query += "GROUP BY a0.URL ORDER BY a0.URL ASC";
-				}
-				
-				//Console.WriteLine(query);
-				command.CommandText = query;
-				reader = command.ExecuteReader();
-				while(reader.Read()) {
-					Console.WriteLine("URL = " + reader["URL"]);
-					//Console.WriteLine("Title = " + reader["Title"]);
-				}
-				reader.Close();
-				Console.WriteLine("Masukkan Query: ");
-				input = Console.ReadLine();
 			}
 		}
 

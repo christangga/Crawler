24cb4d982bc25887a3ab57bfa1b5fa7b6cb9ebb2
Gilang <gilang.9h@gmail.com>
2014-03-27 18:34:59 +0700

    banyak fix

--- WebCrawler/Searcher.cs
+++ WebCrawler/Searcher.cs
@@ -14,7 +14,6 @@ namespace WebCrawler {
 			MySqlDataReader reader;
 			Boolean firstWord;
 			String input, query;
-
 			sql.Open();
 			command = sql.CreateCommand();
 
@@ -23,30 +22,29 @@ namespace WebCrawler {
 			while(!input.Equals("exit")) {
 				firstWord = true;
 				int i = 0;
-				String[] listInput = input.Split(Crawler.delimiterStrings, StringSplitOptions.None);
+				String[] listInput = input.Split(Crawler.delimiterStrings, System.StringSplitOptions.RemoveEmptyEntries);
 				if(listInput.Length == 1) {
 					query = "SELECT URL, Title FROM data WHERE Word LIKE '%" + listInput[0] + "%'";
 					query += "GROUP BY URL ORDER BY URL ASC";
 				}
 				else {
-					query = "SELECT a0.URL, a0.Title FROM ";
+					query = "SELECT * FROM ";
 					for(int j = 1; j < listInput.Length; j++ ) {
 						query += "(";
 					}
-					query += " ((SELECT * FROM data WHERE Word LIKE '%" + listInput[0] + "%') a" + i + ")";
+					query += " ((SELECT URL,Title FROM data WHERE Word LIKE '%" + listInput[0] + "%') a" + i + ")";
 					foreach(String word in listInput){
 						if(firstWord){
 							firstWord = false;
 						}
 						else {
 							i++;
-							query += " INNER JOIN ((SELECT b"+i+".* FROM data b"+i;
-							query += " WHERE Word LIKE '%" + word + "%') a" + i;
+							query += " NATURAL JOIN ((SELECT b"+i+".URL, b"+i+".Title FROM data b"+i;
+							query += " WHERE Word LIKE '%" + word + "%') a" + i + "))";
 							i++;
-							query += " ) ON a0.URL=a" + (i - 1) + ".URL)";
 						}
 					}
-					query += "GROUP BY a0.URL ORDER BY a0.URL ASC";
+					//query += "GROUP BY URL ORDER BY URL ASC";
 				}
 
 				System.Console.WriteLine(query);
